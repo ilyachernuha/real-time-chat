f1e273405961a9d17cdafc6f6b14f5feafe996d3
valentine-ch <valentinvchicherin@gmail.com>
2024-03-08 21:40:12 +0200

    feat: add room tables

--- server/src/db_models.py
+++ server/src/db_models.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, ForeignKey, String, Boolean, DateTime, Integer
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from sqlalchemy.orm import relationship, validates
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import Enum as SQLAlchemyEnum
 from enum import Enum
 from datetime import datetime, timezone
 import secrets
+from room_languages import RoomLanguage
+from room_themes import RoomTheme
 
 
 Base = declarative_base()
@@ -19,6 +21,8 @@ class User(Base):
     name = Column(String, nullable=False)
     account_data = relationship("AccountData", back_populates="user", uselist=False, cascade="all, delete-orphan")
     sessions = relationship("Session", back_populates="user", cascade="all, delete-orphan")
+    rooms_owned = relationship("Room", back_populates="owner", cascade="all, delete-orphan")  # cascade ???
+    rooms = relationship("UserRoomAssociation", back_populates="user", cascade="all, delete-orphan")
 
     @validates("is_guest", "account_data")
     def validate_user(self, key, value):
@@ -135,3 +139,53 @@ class Session(Base):
     device_info = Column(String)
     latest_activity = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     user = relationship("User", back_populates="sessions")
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    room_id = Column(UUID, primary_key=True)
+    owner_id = Column(UUID, ForeignKey("users.user_id"), index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    theme = Column(SQLAlchemyEnum(RoomTheme, name="room_theme"), nullable=False)
+    languages = Column(ARRAY(SQLAlchemyEnum(RoomLanguage, name="room_language")), nullable=False)
+    owner = relationship("User", back_populates="rooms_owned")
+    users = relationship("UserRoomAssociation", back_populates="room", cascade="all, delete-orphan")
+    tags = relationship("RoomTagAssociation", back_populates="room", cascade="all, delete-orphan")
+
+
+class UserRoomAssociation(Base):
+    __tablename__ = "user_room_association"
+
+    user_id = Column(UUID, ForeignKey("users.user_id"), primary_key=True)
+    room_id = Column(UUID, ForeignKey("rooms.room_id"), primary_key=True)
+    is_admin = Column(Boolean, default=False)
+    user = relationship("User", back_populates="rooms")
+    room = relationship("Room", back_populates="users")
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    tag = Column(String, primary_key=True)
+    rooms = relationship("RoomTagAssociation", back_populates="tag")
+    theme = relationship("TagThemeAssociation", back_populates="tag")
+
+
+class RoomTagAssociation(Base):
+    __tablename__ = "room_tag_association"
+
+    room_id = Column(UUID, ForeignKey("rooms.room_id"), primary_key=True)
+    tag_name = Column(String, ForeignKey("tags.tag"), primary_key=True)
+    room = relationship("Room", back_populates="tags")
+    tag = relationship("Tag", back_populates="rooms")
+
+
+class TagThemeAssociation(Base):
+    __tablename__ = "tag_theme_association"
+
+    tag_name = Column(String, ForeignKey("tags.tag"), primary_key=True)
+    theme = Column(SQLAlchemyEnum(RoomTheme, name="room_theme"), primary_key=True)
+    room_count = Column(Integer, nullable=False)
+    tag = relationship("Tag", back_populates="themes")
