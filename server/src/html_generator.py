7c1f7a0a97b06e6b2d07b143b8b8b428d9e57c16
valentine-ch <valentinvchicherin@gmail.com>
2024-04-10 21:35:58 +0300

    use correct urls

--- server/src/html_generator.py
+++ server/src/html_generator.py
@@ -20,12 +20,12 @@ async def generate_register_confirmation_email(code: str, device_info: str):
 
 
 async def generate_reset_password_email(application_id: str):
-    reset_link = base_url + "/reset_password_page/" + application_id
+    reset_link = base_url + "/auth/reset_password_page/" + application_id
     return await template_env.get_template("reset_password_email.html").render_async(reset_link=reset_link)
 
 
 async def generate_reset_password_page(application_id: str):
-    url = base_url + "/finish_reset_password"
+    url = base_url + "/auth/finish_reset_password"
     return await template_env.get_template("reset_password_page.html").render_async(url=url,
                                                                                     application_id=application_id)
 
@@ -35,6 +35,6 @@ async def generate_change_email_confirmation(code: str, username: str):
 
 
 async def generate_change_email_rollback(application_id: str, username: str):
-    rollback_link = base_url + "/rollback_email_change/" + application_id
+    rollback_link = base_url + "/auth/rollback_email_change/" + application_id
     return await template_env.get_template("email_rollback.html").render_async(rollback_link=rollback_link,
                                                                                username=username)
