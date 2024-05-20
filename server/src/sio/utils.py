94b34ec74041000838e9fdb8cf611ac329b50f57
valentine-ch <valentinvchicherin@gmail.com>
2024-05-21 00:12:12 +0300

    refactor: move get_room_sids from external to utils

--- server/src/sio/utils.py
+++ server/src/sio/utils.py
@@ -1,5 +1,14 @@
+from typing import Any
+from .sio import sio
 from .. import db_models
 
 
 async def get_user_room_ids(user: db_models.User):
     return [room.room_id for room in await user.awaitable_attrs.rooms]
+
+
+def get_room_sids(room: Any, namespace: str = "/"):
+    try:
+        return sio.manager.rooms[namespace][room].keys()
+    except KeyError:
+        return []
