caa44753b119ce640adb82436a593efdc96df635
valentine-ch <valentinvchicherin@gmail.com>
2024-06-09 22:54:05 +0300

    refactor: sio exception handling

--- server/src/sio/exception_handlers.py
+++ server/src/sio/exception_handlers.py
@@ -1,24 +1,22 @@
+from socketio.exceptions import ConnectionRefusedError
 import functools
 from typing import Callable
 from sqlalchemy.exc import SQLAlchemyError
-from ..exceptions import FieldSubmitError
+from ..exceptions import (AccessTokenValidationError, BearerTokenExtractionError, MessageValidationError,
+                          FieldSubmitError)
 
 
-def handle_field_submission_error(func: Callable):
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        try:
-            return await func(*args, **kwargs)
-        except FieldSubmitError as e:
-            return "Error", {"detail": e.detail, "field": e.field}
-    return wrapper
-
-
-def handle_sqlalchemy_error(func: Callable):
+def handle_exceptions(func: Callable):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         try:
             return await func(*args, **kwargs)
         except SQLAlchemyError:
             return "Error", {"detail": "Unexpected database error"}
+        except FieldSubmitError as e:
+            return "Error", {"detail": e.detail, "field": e.field}
+        except MessageValidationError as e:
+            return "Error", {"detail": str(e)}
+        except (AccessTokenValidationError, BearerTokenExtractionError) as e:
+            raise ConnectionRefusedError(str(e))
     return wrapper
