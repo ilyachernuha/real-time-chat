bc491eb4a3542b34184b2a779721a983cde42725
Illia Chernukha <ilyachernuha@gmail.com>
2025-03-22 19:32:10 +0200

    fix: auth urls

--- app/src/services/AuthService.ts
+++ app/src/services/AuthService.ts
@@ -9,32 +9,34 @@ import {
 } from "./types";
 import deviceInfo from "@/utils/deviceInfo";
 
+const baseURL = "/auth";
+
 export default {
   register: async (credentials: RegisterCredentials) => {
-    const { data } = await api.post<RegisterResponse>("/create_account", { ...credentials, device_info: deviceInfo });
+    const { data } = await api.post<RegisterResponse>(`${baseURL}/create_account`, { ...credentials, device_info: deviceInfo });
     return data;
   },
   confirmRegistration: async (confirmation: RegisterConfirmation) => {
-    const { data } = await api.post<LoginResponse>("/finish_registration", confirmation);
+    const { data } = await api.post<LoginResponse>(`${baseURL}/finish_registration`, confirmation);
     return data;
   },
   login: async (auth: AxiosBasicCredentials) => {
-    const { data } = await api.post<LoginResponse>("/login", { device_info: deviceInfo }, { auth });
+    const { data } = await api.post<LoginResponse>(`${baseURL}/login`, { device_info: deviceInfo }, { auth });
     return data;
   },
 
   guestLogin: async (name: string) => {
-    const { data } = await api.post<LoginResponse>("/guest_login", { name, device_info: deviceInfo });
+    const { data } = await api.post<LoginResponse>(`${baseURL}/guest_login`, { name, device_info: deviceInfo });
     return data;
   },
 
   forgotPassword: async (email: string) => {
-    const { data } = await api.post<{ status: string }>("/reset_password", { email });
+    const { data } = await api.post<{ status: string }>(`${baseURL}/reset_password`, { email });
     return data;
   },
 
   resetPassword: async (request: ResetPasswordRequest) => {
-    const { data } = await api.put<{ status: string }>("/finish_reset_password", request);
+    const { data } = await api.put<{ status: string }>(`${baseURL}/finish_reset_password`, request);
     return data;
   },
 };
