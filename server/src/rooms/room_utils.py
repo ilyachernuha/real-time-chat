cc825c4b3e55995170e33724ae3cba0110e2aa4d
valentine-ch <valentinvchicherin@gmail.com>
2025-03-24 21:34:36 +0200

    fix: emit notification when added to room

--- server/src/rooms/room_utils.py
+++ server/src/rooms/room_utils.py
@@ -12,6 +12,7 @@ from .schemas import RoomUpdate, UserToAdd
 from ..s3 import S3
 from ..exceptions import FieldSubmitError
 from ..sio import external as sio
+from ..notifications import notification_utils
 
 
 def check_if_room_exists(room: db_models.Room | None):
@@ -218,5 +219,5 @@ async def add_users_to_room(db: AsyncSession, room: db_models.Room, user: db_mod
                             make_admin: bool, adder_id: uuid.UUID):
     await crud.add_user_to_room(db=db, room_id=room.room_id, user=user, make_admin=make_admin)
     await sio.add_user_to_room(user_id=user.user_id, room_id=room.room_id)
-    await crud.create_notification(db=db, user_id=user.user_id, type=db_models.Notification.Type.added_to_room,
-                                   details={"room_id": str(room.room_id), "added_by": str(adder_id)})
+    await notification_utils.create_added_to_room_notification(db=db, user_id=user.user_id, room_id=room.room_id,
+                                                               adder_id=adder_id)
