e75c19ac206a549912cd14d21d0952f9433175ae
Illia Chernukha <ilyachernuha@gmail.com>
2025-04-29 14:04:27 +0000

    fix: enable JSI for WatermelonDB

--- app/src/index.native.ts
+++ app/src/index.native.ts
@@ -1,16 +1,13 @@
-import { Platform } from "react-native";
 import { Database } from "@nozbe/watermelondb";
 import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
 
 import schema, { TableName } from "./model/schema";
 import migrations from "./model/migrations";
 
-// import Post from './model/Post' // ⬅️ You'll import your Models here
 import Message from "@/model/Message";
 import Room from "@/model/Room";
 import User from "@/model/User";
 
-// First, create the adapter to the underlying database:
 const adapter = new SQLiteAdapter({
   schema,
   // (You might want to comment it out for development purposes -- see Migrations documentation)
@@ -19,22 +16,17 @@ const adapter = new SQLiteAdapter({
   // dbName: 'myapp',
   // (recommended option, should work flawlessly out of the box on iOS. On Android,
   // additional installation steps have to be taken - disable if you run into issues...)
-  // jsi: true /* Platform.OS === 'ios' */,
+  jsi: true /* Platform.OS === 'ios' */,
   // (optional, but you should implement this method)
   onSetUpError: (error) => {
     // Database failed to load -- offer the user to reload the app or log out
+    console.error(error);
   },
 });
 
-// Then, make a Watermelon database from it!
 export const db = new Database({
   adapter,
-  modelClasses: [
-    Message,
-    Room,
-    User,
-    // Post, // ⬅️ You'll add Models to Watermelon here
-  ],
+  modelClasses: [Message, Room, User],
 });
 
 export const roomsCollection = db.get<Room>(TableName.ROOMS);
