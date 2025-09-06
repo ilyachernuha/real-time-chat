b838a7d3fe118e2e46f5dc7d0f0d0ff50ae50191
Illia Chernukha <ilyachernuha@gmail.com>
2025-09-06 14:26:40 +0000

    fix: image attachment aspect ratio

--- app/src/features/rooms/screens/ChatRoomScreen.tsx
+++ app/src/features/rooms/screens/ChatRoomScreen.tsx
@@ -41,7 +41,7 @@ export const ChatRoomScreenBase = ({ room }: Props) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       allowsEditing: true,
-      // aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
